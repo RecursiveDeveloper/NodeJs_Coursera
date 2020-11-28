f3a718aae987af19a0c4f507a0d359b8fb710246
Jhoan Ortiz S <johanjesus133@hotmail.com>
2020-11-27 22:15:08 -0500

    Cambios en markers y logo pagina principal

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('main_map').setView([7.0675745,-73.1445733], 13);
+var map = L.map('main_map').setView([7.0613441,-73.0828505,12], 13);
 
 //Obtenido de -> https://leafletjs.com/
 
@@ -6,10 +6,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([7.0675740,-73.1445723]).addTo(map)
+L.marker([7.074291, -73.095725]).addTo(map)
     .bindPopup('Bicicleta #1.')
     .openPopup();
 
-L.marker([7.042500,-73.1345100]).addTo(map)
+L.marker([7.058004, -73.087066]).addTo(map)
     .bindPopup('Bicicleta #2.')
+    .openPopup();
+
+L.marker([7.087353, -73.109521]).addTo(map)
+    .bindPopup('Bicicleta #3.')
+    .openPopup();
+
+L.marker([7.075564, -73.091438]).addTo(map)
+    .bindPopup('Bicicleta #4.')
     .openPopup();
