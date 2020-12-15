0e73ba74ba760b44f4f71ceff063d246c80291fc
Jhoan Ortiz S <johanjesus133@hotmail.com>
2020-12-15 09:22:44 -0500

    Sengrid config

--- red-bicicletas/mailer/mailer.js
+++ red-bicicletas/mailer/mailer.js
@@ -1,12 +1,37 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'paula26@ethereal.email',
-        pass: 'zjg1VTJqZZNkzhAkCT'
+let mailConfig;
+if(process.env.NODE_ENV === 'production') {
+    const options = {
+        auth: {
+            api_key: process.env.SENGRID_API_SECRET
+        }
+    }
+    mailConfig = sgTransport(options);
+}else{
+    if(process.env.NODE_ENV === 'staging') {
+        console.log('XXXXXXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+    }else{
+        const mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.etheral_user,
+                pass: process.env.etheral_pwd
+                /*user: 'paula26@ethereal.email',
+                pass: 'zjg1VTJqZZNkzhAkCT'*/
+            }
+        }
     }
 }
+
+
 
 module.exports = nodemailer.createTransport(mailConfig);
